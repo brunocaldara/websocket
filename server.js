666a3e5901dec16201daa42bda1d032a65429dc0
Bruno Caldara <bruno.caldara@pci.es.gov.br>
2024-10-18 12:11:01 -0300

    Commit Inicial

---
+++ server.js
@@ -0,0 +1,19 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const socket = new Server(httpServer, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
+});
+
+socket.on("connection", (socket) => {
+  console.log(socket);
+
+  socket.emit("message", "Hello");
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is connected");
+});
